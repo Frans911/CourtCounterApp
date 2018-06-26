41980e9d1deb1319d06665c7840e2d93ad28bf33
Mahlangu1993 <212281972@tut4life.ac.za>
2018-06-26 13:45:09 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,36 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  scoreA:number;
+  scoreB:number;
   constructor(public navCtrl: NavController) {
+    this.scoreA = 0;
+    this.scoreB = 0
+  }
 
+  TeamAadd(num:number){
+    if(num == 3){
+      this.scoreA += 3;
+    }else  if(num == 2){
+      this.scoreA += 2;
+    }
+    if(num == 1){
+      this.scoreA += 1;
+    }
+  }
+  TeamBadd(num:number){
+    if(num == 3){
+      this.scoreB += 3;
+    }else  if(num == 2){
+      this.scoreB += 2;
+    }
+    if(num == 1){
+      this.scoreB += 1;
+    }
+  }
+  reset(){
+    this.scoreA = 0;
+    this.scoreB = 0;
   }
 
 }
